912c983ce157f7c12e98755940e1b2d03c4974ee
danieljhegeman <student@hackreactor.com>
2018-02-08 11:04:00 -0800

    Completed basic functionality

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -27,6 +27,7 @@ exports.initialize = function(pathsObj) {
 
 var urlPath = exports.paths.list;
 var archivePath = exports.paths.archivedSites;
+var loadingPath = exports.paths.siteAssets + '/loading.html';
 
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
@@ -37,8 +38,8 @@ exports.readListOfUrls = function(callback) {
       console.log(err, ' = Readfile error');
     }
     if (data) {
-      list = data.substring(0, data.length - 1).split('~').map(function(urlTup) {
-        return JSON.parse(urlTup);
+      list = data.substring(0, data.length - 1).split('~').map(function(url) {
+        return JSON.parse(url);
       });
     } else {
       list = [];
@@ -52,8 +53,8 @@ exports.readListOfUrls = function(callback) {
 exports.isUrlInList = function(url, callback) {
   var checkUrl = function(urlList) {
     var bool = _.some(urlList, function(item) {
-      console.log('list url = ', item[0], 'passed in url = ', url);
-      return item[0] === url;
+      console.log('list url = ', item, 'passed in url = ', url);
+      return item === url;
     });
     if (callback) {
       callback(bool);
@@ -65,8 +66,7 @@ exports.isUrlInList = function(url, callback) {
 exports.addUrlToList = function(url, callback) {
   var writeUrl = function(isUrl) {
     if (!isUrl) {
-      var urlTup = [url, false];
-      fs.appendFile(urlPath, JSON.stringify(urlTup) + '~');
+      fs.appendFile(urlPath, JSON.stringify(url) + '~');
       console.log('Added URL to list');
     } else {
       console.log('URL already in list.');
@@ -76,11 +76,27 @@ exports.addUrlToList = function(url, callback) {
 };
 
 exports.isUrlArchived = function(url, callback) {
-  fs.lstat(archivePath + url, callback);
+  var shortUrl = url;
+  if (url.substring(0, 7) === 'http://') {
+    shortUrl = url.substring(7);
+  }
+  fs.lstat(archivePath + '/' + shortUrl, function(err, stats) {
+    if (err) {
+      console.log('Error reading archived page: ', err);
+      if (callback) {
+        callback(url, false);
+      }
+    } else {
+      callback(url, true);
+    }
+  });
 };
 
-exports.writePage = function(website, name) {
-  fs.writeFile(archivePath + name, website, function(err) {
+exports.writePage = function(website, url) {
+  if (url.substring(0, 7) === 'http://') {
+    url = url.substring(7);
+  }
+  fs.writeFile(archivePath + '/' + url, website, function(err) {
     if (err) {
       console.log('Error: ', err);
     }
@@ -89,45 +105,50 @@ exports.writePage = function(website, name) {
 };
 
 exports.downloadUrls = function(urls) {
+  var download = function(url, isArchived) {
+    if (!isArchived) {
+      fetcher.fetchPage(url, (website) => {
+        exports.writePage(website, url);
+      });
+    } 
+  };
+  
   urls.forEach(function(url) {
-    fetcher.fetchPage(url, (website) => {
-      exports.writePage(website, url);
-    });
+    exports.isUrlArchived(url, download);
   });
 };
 
-exports.readPage = function(webpage, res, callback) {
-  if (!req) {
-    res.writeHead(404);
-    res.end();
-  }
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(webpage);
-  res.end();
-};
 
-exports.renderPage = function(url, res, err, stats) {
-  if (err) {
-    exports.addUrlToList(url);
+exports.renderPage = function(res, url, isArchived) {
+  var shortUrl = url;
+  if (url.substring(0, 7) === 'http://') {
+    shortUrl = url.substring(7);
+  }
+  if (isArchived) {
+    fs.readFile(archivePath + '/' + shortUrl, function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
   } else {
-    fs.readFile(archivePath + url, function(err, data) {
-      if (err) {
-        exports.readPage(null, req);
-      } else {
-        exports.readPage(data, req);
-      }
+    exports.addUrlToList(url);
+    fs.readFile(loadingPath, function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
     });
   }
 };
 
 exports.requestPage = function(url, res) {
-  exports.isUrlArchived(url, exports.renderPage.bind(this, url, res));
+  exports.isUrlArchived(url, exports.renderPage.bind(this, res));
 };
 
 new CronJob('* * * * * *', function() {
   console.log('Job every minute');
   var unarchived = [];
   var getPages = function(list) {
-    downloadUrls(list);
+    exports.downloadUrls(list);
   };
+  exports.readListOfUrls(getPages);
 }, null, true, 'America/Los_Angeles');
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -3,8 +3,11 @@ const request = require('request');
 
 
 var fetchPage = function(url, callback) {
+  if (url.substring(0, 7) !== 'http://') {
+    url = 'http://' + url;
+  }
   request(url, (err, res, body) => {
-    err ? reject(err) : callback(body);
+    err ? console.log(err) : callback(body);
   });
 };
 
